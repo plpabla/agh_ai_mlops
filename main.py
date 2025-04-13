cb2bb688342d120a62eabf2da8eb5964575b3f4f
pawel-laptop <p.blaszczy@gmail.com>
2025-04-13 13:53:28 +0200

    add secrets manager

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import os
 import argparse
+import yaml as pyyaml
 from dotenv import load_dotenv
 from settings import Settings
 
 
-def export_envs(environment: str = "dev") -> None:
+def export_envs(environment: str = "dev", secrets: str = "secrets.yaml") -> None:
     env_file = f".env.{environment}"
 
     if not os.path.exists(env_file):
         print(f"Warning: Environment file {env_file} does not exist.")
         return
     load_dotenv(env_file)
+
+    # Load secrets from YAML file
+    if os.path.exists(secrets):
+        with open(secrets, "r") as file:
+            secrets_data = pyyaml.safe_load(file)
+            for key, value in secrets_data.items():
+                os.environ[key] = str(value)
+    else:
+        print(f"Warning: Secrets file {secrets} does not exist.")
 
 
 if __name__ == "__main__":
@@ -31,3 +41,4 @@ if __name__ == "__main__":
 
     print("APP_NAME: ", settings.APP_NAME)
     print("ENVIRONMENT: ", settings.ENVIRONMENT)
+    print("FASTAPI_SECRET: ", settings.FASTAPI_SECRET)
--- settings.py
+++ settings.py
@@ -5,9 +5,16 @@ from pydantic import field_validator
 class Settings(BaseSettings):
     ENVIRONMENT: str
     APP_NAME: str
+    FASTAPI_SECRET: str
 
     @field_validator("ENVIRONMENT")
     def validate_environment(cls, value):
         if value not in ["dev", "test", "prod"]:
             raise ValueError("ENVIRONMENT must be one of 'dev', 'test', or 'prod'")
+        return value
+
+    @field_validator("FASTAPI_SECRET")
+    def validate_fastapi_secret(cls, value):
+        if len(value) < 32:
+            raise ValueError("FASTAPI_SECRET must be at least 32 characters long")
         return value
